3f52d6cd7b3acc8a93bbed8eff7d30241cb7d1cb
gabri10on <gabriel.r_00@hotmail.com>
2023-01-17 19:22:50 -0300

    tentativa requisito 20

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const button = document.getElementById('button-form');
 const btnSubmit = document.getElementById('submit-btn');
 const checkbox = document.getElementById('agreement');
+const textarea = document.getElementById('textarea');
 
 function checkForm() {
   const email = document.getElementById('email');
@@ -23,6 +24,14 @@ function activeBtn() {
 }
 
 checkbox.addEventListener('click', activeBtn);
+
+//tentativa requisito 20
+
+// function contagem(event) {
+//   let cont = textarea.value.length;
+// }
+
+// textarea.addEventListener('input', contagem);
 
 window.onload = () => {
   btnSubmit.disabled = true;
