116904a2120dfd4c9b7909d3d8104471c15c761d
gabri10on <gabriel.r_00@hotmail.com>
2023-01-17 18:41:57 -0300

    Requisito 18 concluído

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const button = document.getElementById('button-form');
+const btnSubmit = document.getElementById('submit-btn');
+const checkbox = document.getElementById('agreement');
+
 function checkForm() {
   const email = document.getElementById('email');
   const password = document.getElementById('password');
@@ -10,3 +13,17 @@ function checkForm() {
 }
 
 button.addEventListener('click', checkForm);
+
+function activeBtn() {
+  if (checkbox.checked) {
+    btnSubmit.disabled = false;
+  } else {
+    btnSubmit.disabled = true;
+  }
+}
+
+checkbox.addEventListener('click', activeBtn)
+
+window.onload = () => {
+  btnSubmit.disabled = true;
+}
