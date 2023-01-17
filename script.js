c15bdb957c9236e1a10941b245ec0ee9fd76d5f8
Douglas Oliveira <doglasolivera13@gmail.com>
2023-01-17 15:51:06 -0300

    Requisito 3 concluido

---
+++ script.js
@@ -0,0 +1,12 @@
+const button = document.getElementById('button-form');
+function checkForm() {
+  const email = document.getElementById('email');
+  const password = document.getElementById('password');
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    window.alert('Olá, Tryber!');
+  } else {
+    window.alert('Email ou senha inválidos.');
+  }
+}
+
+button.addEventListener('click', checkForm);
