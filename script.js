19108f8379dc95423abf76c4c15e8a87a78a571b
Douglas Oliveira <doglasolivera13@gmail.com>
2023-01-18 09:20:51 -0300

    Requisito 21 concluido

--- script.js
+++ script.js
@@ -37,3 +37,74 @@ window.onload = () => {
   btnSubmit.disabled = true;
   contadora.innerHTML = 500;
 };
+
+function createParagraph(content) {
+  const paragraph = document.createElement('p');
+  paragraph.innerHTML = content;
+  return paragraph;
+}
+const formData = document.createElement('section');
+formData.id = 'form-data';
+
+function collectFullname() {
+  const firstname = document.getElementById('input-name').value;
+  const lastname = document.getElementById('input-lastname').value;
+  const fullname = `Nome: ${firstname} ${lastname}`;
+  formData.appendChild(createParagraph(fullname));
+}
+
+function collectEmail() {
+  const email = `Email: ${document.getElementById('input-email').value}`;
+  formData.appendChild(createParagraph(email));
+}
+
+function collectHouse() {
+  const house = `Casa: ${document.getElementById('house').value}`;
+  formData.appendChild(createParagraph(house));
+}
+
+function collectFamily() {
+  const family = `Família: ${document.getElementById('evaluation-form').family.value}`;
+  formData.appendChild(createParagraph(family));
+}
+
+function collectMessage() {
+  const message = `Observações: ${document.getElementById('textarea').value}`;
+  formData.appendChild(createParagraph(message));
+}
+
+function collectSkills() {
+  const checkboxes = document.querySelectorAll('.subject');
+  const skills = [];
+  for (let i = 0; i < checkboxes.length; i += 1) {
+    if (checkboxes[i].checked) {
+      skills.push(checkboxes[i].value);
+    }
+  }
+  const skill = `Matérias: ${skills.join(', ')}`;
+  formData.appendChild(createParagraph(skill));
+}
+
+function collectAvaliation() {
+  const avaliation = `Avaliação: ${document.querySelector('input[name=\'rate\']:checked').value}`;
+  formData.appendChild(createParagraph(avaliation));
+}
+
+function showUserData() {
+  const page = document.getElementById('main-content');
+  const form = document.getElementById('evaluation-form');
+  form.style.display = 'none';
+  page.insertBefore(formData, document.getElementById('trybewarts-forms-logo'));
+}
+
+btnSubmit.addEventListener('click', (event) => {
+  event.preventDefault();
+  collectFullname();
+  collectEmail();
+  collectHouse();
+  collectFamily();
+  collectSkills();
+  collectAvaliation();
+  collectMessage();
+  showUserData();
+});
